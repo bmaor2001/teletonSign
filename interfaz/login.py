4a697a89b4a9f199daad2a9977fb2926a295aeef
IsaacArredondoPadron <73501289+IsaacArredondoPadron@users.noreply.github.com>
2022-04-21 15:24:57 -0500

    cambie la imagen de fondo

--- interfaz/login.py
+++ interfaz/login.py
@@ -40,11 +40,6 @@ def verify_to_options():
 def request_signature_to_options():
     request_signature.state(newstate = "withdraw")
     options_window()
-    
-def open_file():
-    nombrearch=fd.askopenfilename(initialdir = "/",title = "Seleccione archivo",filetypes = (("pdf files","*.pdf"),("todos los archivos","*.*")))
-    print(nombrearch)
-    return nombrearch
 
 ## FUNCIONES DE VENTANAS
 
@@ -122,6 +117,21 @@ def options_window():
 
 # VENTANA PARA FIRMAR DOCUMENTOS
 def sign_window():
+    
+    def select_file():
+        global nombrearch_to_sign
+        nombrearch_to_sign=fd.askopenfilename(initialdir = "/",title = "Seleccione archivo",filetypes = (("pdf files","*.pdf"),("todos los archivos","*.*")))
+        print(nombrearch_to_sign)
+    
+    def select_privateKey():
+        global privateKey
+        privateKey=fd.askopenfilename(initialdir = "/",title = "Seleccione archivo",filetypes = (("pdf files","*.pdf"),("todos los archivos","*.*")))
+        print(privateKey)
+        
+    def sign_file():
+        print("File Signed " + "privateKey: " + privateKey + " name: " + nombrearch_to_sign)
+        sign_to_options()
+    
     global sign
     sign = Toplevel(options)
     sign.title("Sign")
@@ -134,23 +144,39 @@ def sign_window():
     btn_home = Button(sign, cursor="hand2", bg=yellow_color, width=1, relief="flat", padx=8, pady=1, activebackground=yellow_color, command=sign_to_options)
     btn_home.place(x=191, y=127)
     
-    #Boton de document
+    #Boton de cargar document
     btn_Document = Button(sign, text="Document", cursor="hand2", bg=purple_color, fg=white_color, width=12, relief="flat",
-                      font=("Poppins Light", 14, "bold"), padx=85, pady=10)
+                      font=("Poppins Light", 14, "bold"), padx=85, pady=10, command=select_file)
     btn_Document.place(x=34, y=246)
     #Boton de private key
     btn_PrivateKey = Button(sign, text="Private Key", cursor="hand2", bg=purple_color, fg=white_color, width=12, relief="flat",
-                    font=("Poppins Light", 14, "bold"), padx=85, pady=10)
+                    font=("Poppins Light", 14, "bold"), padx=85, pady=10, command=select_privateKey)
     btn_PrivateKey.place(x=34, y=358)
     #Boton de firmar
     btn_Sign = Button(sign, text="Sign", cursor="hand2", bg=purple_color, fg=white_color, width=12, relief="flat",
-                    font=("Poppins Light", 14, "bold"), padx=85, pady=10)
+                    font=("Poppins Light", 14, "bold"), padx=85, pady=10, command=sign_file)
     btn_Sign.place(x=34, y=469)
     
     sign.mainloop()
 
 # FUNCION PARA VERIFICAR DOCUMENTOS
 def verify_window():
+    
+    def select_file():
+        global nombrearch_to_verify
+        nombrearch_to_verify=fd.askopenfilename(initialdir = "/",title = "Seleccione archivo",filetypes = (("pdf files","*.pdf"),("todos los archivos","*.*")))
+        print(nombrearch_to_verify)
+        
+    def select_publicKey():
+        global publicKey
+        publicKey=fd.askopenfilename(initialdir = "/",title = "Seleccione archivo",filetypes = (("pdf files","*.pdf"),("todos los archivos","*.*")))
+        print(publicKey)
+        
+    def verify_file():
+        print("File Verified " + "publicKey: " + publicKey + " name: " + nombrearch_to_verify)
+        verify_to_options()
+    
+    
     global verify
     verify = Toplevel(options)
     verify.title("Verify")
@@ -165,21 +191,30 @@ def verify_window():
     
     #Boton de document
     btn_Document = Button(verify, text="Document", cursor="hand2", bg=purple_color, fg=white_color, width=12, relief="flat",
-                      font=("Poppins Light", 14, "bold"), padx=85, pady=10)
+                      font=("Poppins Light", 14, "bold"), padx=85, pady=10, command=select_file)
     btn_Document.place(x=34, y=246)
-    #Boton de private key
-    btn_PrivateKey = Button(verify, text="Private Key", cursor="hand2", bg=purple_color, fg=white_color, width=12, relief="flat",
-                    font=("Poppins Light", 14, "bold"), padx=85, pady=10)
+    #Boton de public key
+    btn_PrivateKey = Button(verify, text="Public Key", cursor="hand2", bg=purple_color, fg=white_color, width=12, relief="flat",
+                    font=("Poppins Light", 14, "bold"), padx=85, pady=10, command=select_publicKey)
     btn_PrivateKey.place(x=34, y=358)
     #Boton de Verificar
     btn_Verify = Button(verify, text="Verify", cursor="hand2", bg=purple_color, fg=white_color, width=12, relief="flat",
-                    font=("Poppins Light", 14, "bold"), padx=85, pady=10)
+                    font=("Poppins Light", 14, "bold"), padx=85, pady=10, command=verify_file)
     btn_Verify.place(x=34, y=469)
     
     verify.mainloop()
 
 # FUNCION PARA SOLICITAR FIRMAR
 def request_signature_window():
+    def select_file():
+        global nombrearch
+        nombrearch=fd.askopenfilename(initialdir = "/",title = "Seleccione archivo",filetypes = (("pdf files","*.pdf"),("todos los archivos","*.*")))
+        print(nombrearch)
+    
+    def nominas(tagNominas, typoDocument, description, tagsDocuments):
+        print("Las nominas son: " + tagNominas.get() + ", el typo es: " + typoDocument.get() + ", su descripcion es: " + description.get() + " y sus tags son: " + tagsDocuments.get() + " y la direccion del archivo es: " + nombrearch)
+        request_signature_to_options()
+    
     global request_signature
     request_signature = Toplevel(options)
     request_signature.title("Request Signature")
@@ -188,40 +223,48 @@ def request_signature_window():
     fondo = PhotoImage(file="request.png")
     fondo1 = Label(request_signature, image=fondo).place(x=0, y=0, relwidth=1, relheight=1)
     
-    #Variables
+    #VARIABLES
     tagNominas = StringVar()
+    typoDocument = StringVar()
+    description = StringVar()
+    tagsDocument = StringVar()
+    nominas = partial(nominas, tagNominas, typoDocument, description, tagsDocument)
     
+    # ENTRADAS
+    #Entrada para tomar el typo de documento
+    entrada_typo = Entry(request_signature, font=("Poppins", 12, "italic"), textvariable=typoDocument, width=28, relief="flat", bg=purple_color, bd=20, fg=white_color, insertbackground=white_color)
+    entrada_typo.place(x=34,y=320)
+    entrada_typo.insert(0, "write typo...")
+    
+    #Entrada para tomar la descripcion del documento
+    entrada_description = Entry(request_signature, font=("Poppins", 12, "italic"), textvariable=description, width=28, relief="flat", bg=purple_color, bd=20, fg=white_color, insertbackground=white_color)
+    entrada_description.place(x=34,y=395)
+    entrada_description.insert(0, "write description...")
+    
+    #Entrada para tomar los tags del documento
+    entrada_tagsDocument = Entry(request_signature, font=("Poppins", 12, "italic"), textvariable=tagsDocument, width=28, relief="flat", bg=purple_color, bd=20, fg=white_color, insertbackground=white_color)
+    entrada_tagsDocument.place(x=34,y=475)
+    entrada_tagsDocument.insert(0, "write document's tags...")
+    
+    #Entrada para toma de nominas que deben firmar
+    entrada_tags = Entry(request_signature, font=("Poppins", 12, "italic"), textvariable=tagNominas, width=28, relief="flat", bg=purple_color, bd=20, fg=white_color, insertbackground=white_color)
+    entrada_tags.place(x=34,y=590)
+    entrada_tags.insert(0, "write payrolls here...")
+
+    #BOTONES
     #Boton de Home
     btn_home = Button(request_signature, cursor="hand2", bg=yellow_color, width=1, relief="flat", padx=8, pady=1, activebackground=yellow_color, command=request_signature_to_options)
     btn_home.place(x=191, y=127)
     
-    #Boton de document
+    #Boton de cargar documento
     btn_Document = Button(request_signature, text="Document", cursor="hand2", bg=purple_color, fg=white_color, width=12, relief="flat",
-                      font=("Poppins Light", 14, "bold"), padx=85, pady=10, command=open_file)
+                      font=("Poppins Light", 14, "bold"), padx=85, pady=10, command=select_file)
     btn_Document.place(x=34, y=246)
-    #Boton de private key
-    '''btn_Tags = Button(request_signature, text="Tags", cursor="hand2", bg=purple_color, fg=white_color, width=12, relief="flat",
-                    font=("Poppins Light", 14, "bold"), padx=85, pady=10)
-    btn_Tags.place(x=34, y=358)'''
-    #Boton para entrada de nominas que deben firmar
-    entrada_tags = Entry(request_signature, font=("Poppins", 12, "italic"), textvariable=tagNominas, width=28, relief="flat", bg=purple_color, bd=20, fg=white_color, insertbackground=white_color)
-    entrada_tags.place(x=34,y=358)
-    entrada_tags.insert(0, "write payrolls here...")
-    entrada_tags.focus_set()
     
-    def placeholder(event):
-        entrada_tags.configure(state = NORMAL)
-        entrada_tags.configure(font=("Poppins", 12, "bold"))
-        entrada_tags.delete(0, END)
-
-        entrada_tags.unbind('<Button-1>', click)
-
-    click = entrada_tags.bind('<Button-1>', placeholder)
-    
-    #Boton de Verificar
+    #Boton de Solicitar
     btn_Request_Signature = Button(request_signature, text="Request Signature", cursor="hand2", bg=purple_color, fg=white_color, width=12, relief="flat",
-                    font=("Poppins Light", 14, "bold"), padx=85, pady=10)
-    btn_Request_Signature.place(x=34, y=469)
+                    font=("Poppins Light", 14, "bold"), padx=85, pady=10, command=nominas)
+    btn_Request_Signature.place(x=34, y=703)
     
     request_signature.mainloop()
     
