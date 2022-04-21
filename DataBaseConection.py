2b7d3bbb35f4f830e1cd2b8f76bd62f5492c49c4
Jairo Enrique <A01750443@itesm.mx>
2022-04-21 08:40:57 -0500

    act

---
+++ DataBaseConection.py
@@ -0,0 +1,94 @@
+
+import pymysql
+
+class DataBase:
+    def __init__(self, user, password, db):
+        self.connection = pymysql.connect(
+            host = "localhost",
+            user = user,
+            password = password,
+            db = db
+        )
+        self.cursor = self.connection.cursor()
+    
+        print("Conexión exitosa")
+    
+    def insert_users(self, nomina, nombre, puesto, tags, certificado, estatus):
+            certificado = open(certificado, "rb").read()
+            sql = """INSERT INTO users (`Nomina`, `Nombre`, `Puesto`, `Tags`, `Certificado`, `Estatus`) VALUES (%s,%s,%s,%s,%s,%s)"""
+            try:
+                self.cursor.execute(sql, (nomina, nombre, puesto, tags, certificado, estatus))
+                print(f"{nombre} ha sido añadido a la base de datos.")
+            except Exception as e:
+                print(e)
+                
+    def insert_documentos(self, Hash, Tipo, Nombre, Descripcion, Tags, Estatus):
+            sql = f"INSERT INTO documentos (`Hash`, `Tipo`, `Nombre`, `Descripcion`, `Tags`, `Estatus`) VALUES ('{Hash}', '{Tipo}', '{Nombre}', '{Descripcion}', '{Tags}', '{Estatus}')"
+            try:
+                self.cursor.execute(sql)
+                print(f"{Nombre} ha sido añadido a la base de datos.")
+            except Exception as e:
+                print(e)
+                
+    def insert_firma(self, Hash, Nomina): 
+            #Doc_signed = open(Doc_signed, "rb").read()
+            sql = """ INSERT INTO firmas  (Hash, Nomina) VALUES (%s,%s)"""
+            try:
+                self.cursor.execute(sql, (Hash, Nomina))
+                print(f"{Hash} ha sido añadido a la base de datos.")
+            except Exception as e:
+                print(e)
+    
+    def cargar_firma(self, Doc_signed, Hash, Nomina):
+        Doc_signed = open(Doc_signed, "rb").read()
+        sql = """ UPDATE firmas SET Doc_signed = %s WHERE (Hash, Nomina) = (%s, %s)"""
+        self.cursor.execute(sql, (Doc_signed, Hash, Nomina))
+                
+    def select(self, tabla, what, where, value, where_2 = "", value_2 = ""):
+        if what == "*":
+            sql = f"SELECT {what} FROM {tabla}"
+        elif where_2 != "":
+            sql = f"SELECT {what} FROM {tabla} where {where} = '{value}' AND {where_2} = '{value_2}'"
+            
+        else:
+            sql = f"SELECT {what} FROM {tabla} where {where} = '{value}'"
+        self.cursor.execute(sql)
+        val = self.cursor.fetchall()
+        return val
+        
+    def update(self, tabla, what, value, where_col, where_val):
+        sql = f"UPDATE {tabla} set {what} = '{value}' where {where_col} = '{where_val}'"
+        self.cursor.execute(sql)
+        print("Actualización exitosa")
+        
+    def delete(self, tabla, where, value):
+        sql = f"DELETE FROM {tabla} where {where} = '{value}'"
+        self.cursor.execute(sql)
+        print("Eliminación exitosa")
+        
+    def create(self, password):
+                Users_table = "CREATE TABLE `users` (`Nomina` varchar(10) NOT NULL,`Nombre` varchar(100) NOT NULL,`Puesto` varchar(50) NOT NULL,`Tags` text NOT NULL,`Certificado` blob,`Estatus` varchar(20) NOT NULL, PRIMARY KEY (`Nomina`))"
+                Documentos_table = "CREATE TABLE `documentos` (`Hash` varchar(80) NOT NULL,`Tipo` varchar(100) NOT NULL,`Nombre` varchar(100) NOT NULL,`Descripcion` text NOT NULL,`Tags` varchar(100) NOT NULL,`Estatus` varchar(10) NOT NULL,`TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (`Hash`))"
+                Firmas_table = "CREATE TABLE `firmas` (`ID` int(11) NOT NULL AUTO_INCREMENT,`Hash` varchar(100) NOT NULL,`Doc_signed` blob,`Nomina` varchar(50) NOT NULL,`TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`ID`))"
+                connection = pymysql.connect(
+                        host = "localhost",
+                        user = "root",
+                        password = "",
+                        db = "sys"
+                    )
+                cursor = connection.cursor()
+                cursor.execute("CREATE DATABASE teleton")
+                cursor.close()
+
+                connection = pymysql.connect(
+                            host = "localhost",
+                            user = "root",
+                            password = "",
+                            db = "teleton"
+                        )
+                cursor = connection.cursor()
+                cursor.execute(Firmas_table)
+                cursor.execute(Documentos_table)
+                cursor.execute(Users_table)
+                self.cursor = self.connection.cursor()
+
